50d4c9da9525ddcce6dab5b8ddf3d99c32f6c0aa
Higaki-Ryota <higaki-ryota589@g.ecc.u-tokyo.ac.jp>
2025-11-11 01:28:45 +0900

    nuxt config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,11 @@ import { join } from 'path'
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  css:[ 'katex/dist/katex.min.css', "@/assets/styles/common.scss"],
+
+  css: [
+    'katex/dist/katex.min.css',
+    "@/assets/styles/common.scss",
+  ],
 
   vite: {
     assetsInclude: ['**/*.woff2']
@@ -18,11 +22,17 @@ export default defineNuxtConfig({
 
   ssr: false,
 
+  nitro: {
+    preset: "github-pages",
+  },
+
   app: {
-    baseURL: '/MathLab-Quiz/',  
-    buildAssetsDir: 'assets/',
-    head: {                               
-      base: { href: '/MathLab-Quiz/' }
-    }
-  }
+    baseURL: "/MathLab_Quiz/",  // ★ リポジトリ名と一致
+    buildAssetsDir: "_nuxt/",   // ★ Nuxt の正しいデフォルト
+  },
+
+  generate: {
+    dir: "dist",
+    fallback: true,
+  },
 })
