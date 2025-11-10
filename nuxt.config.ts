2fe85242e24b08af796bd17954698c668f6e0b40
Higaki-Ryota <higaki-ryota589@g.ecc.u-tokyo.ac.jp>
2025-11-11 01:15:39 +0900

    nuxt config.tsの変更など

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,28 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 import { writeFileSync } from 'fs'
 import { join } from 'path'
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   css:[ 'katex/dist/katex.min.css', "@/assets/styles/common.scss"],
+
   vite: {
     assetsInclude: ['**/*.woff2']
   },
+
   hooks: {
     'generate:done'() {
-      const distDir = join(process.cwd(), 'docs')
+      const distDir = join(process.cwd(), 'dist')
       writeFileSync(join(distDir, '.nojekyll'), '')
     }
   },
-  devtools: { enabled: true },
-  target: 'static',
-  router: {
-    base: '/Mathlab-Quiz/'
-  },
-  generate: {
-    dir: 'docs', // 出力先フォルダを 'dist' から 'docs' に変更
-    fallback: true, // SPAモード用に404.htmlを生成
-  },
+
   ssr: false,
+
   app: {
-    baseURL: '/Mathlab-Quiz/',
+    baseURL: '/Mathlab-Quiz/',  
+    buildAssetsDir: 'assets/',
+    head: {                               
+      base: { href: '/Mathlab-Quiz/' }
+    }
   }
-});
+})
